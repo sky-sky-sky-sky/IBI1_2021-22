dc1e4e570eb2b74157dd05ba44dd5e9525a1dfe4
sky-sky-sky-sky <101196695+sky-sky-sky-sky@users.noreply.github.com>
2022-03-18 16:05:11 +0800

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,11 @@
+#this date means 7 March.
+
+a=19245301 #there	 had been	 a total 19,245,301 cases of COVID-19 in the UK since the pandemic began.
+b=4218520  #On this date in 2021 there had been a total of 4,218,520 cases.
+c=271           #On this date in 2020 there had been 271 cases.
+d=b-c          #Calculate the difference between the numbers of cases in 2020 and 2021.
+e=a-b          #Calculate the difference between the numbers of cases in 2021 and 2022.
+if d>e:
+	print('d is greater')
+if e>d:
+	print('e is greater')
