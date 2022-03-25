93552dd6bffd2e1502ff5eb676eb678b1c29124e
sky-sky-sky-sky <101196695+sky-sky-sky-sky@users.noreply.github.com>
2022-03-25 11:22:47 +0800

    Delete Practical6 directory

--- Practical6/congenital heart disease.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-N = 10
-paternal_age=[30,35,40,45,50,55,60,65,70,75]
-chd=[1.03,1.07,1.11,1.17,1.23,1.32,1.42,1.55,1.72,1.94]
-plt.scatter(paternal_age,chd) 
-
-plt.show()
