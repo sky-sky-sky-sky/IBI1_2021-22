db7434b806955dac7727a216d6cf3c5c1a7c61b8
sky-sky-sky-sky <101196695+sky-sky-sky-sky@users.noreply.github.com>
2022-05-24 23:26:01 +0800

    Add files via upload

---
+++ Practical6change/marks.py
@@ -0,0 +1,16 @@
+from turtle import width
+import numpy as np
+import matplotlib.pyplot as plt
+
+marks=[45,36,86,57,53,92,65,45]
+marks.sort()
+print(marks)
+pl = plt.boxplot(marks)
+plt.ylabel("masks") 
+plt.show()
+
+n=(45+36+86+57+53+92+65+45)/8#Calculate the average score
+if n>=60:#discuss whether the score is higher than 60
+    print('yes, this is higher than the pass mark of 60%')
+if n<60:
+    print('no,this is not higher than the pass mark of 60%')
